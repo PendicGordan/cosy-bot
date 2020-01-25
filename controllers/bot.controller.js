bd60e82701957ef70c168c8d1b868167975919f2
Gordan Pendic <gordan.pendic5@gmail.com>
2020-01-25 16:54:39 +0100

    ids added

--- controllers/bot.controller.js
+++ controllers/bot.controller.js
@@ -14,18 +14,38 @@ module.exports.hello = hello;
 const getCompanies = async function(req, res){
 	const companies = [
 		{
+			id: 1,
 			name: 'MOZAIK Cafe&Restaurant',
 			address: 'Stuwerstrasse 20/18, 1020 Wien',
 			image: 'https://cdn.werbifi.com/3/image6.jpg',
 			rating: 5,
 		},
 		{
+			id: 2,
 			name: 'Schweizerhaus',
 			address: 'Prater 116, 1020 Wien',
 			image: 'https://www.vienna.at/2019/10/Schweizerhaus09-4-3-590945709-945x709.jpg',
+			rating: 4,
+		},
+		{
+			id: 3,
+			name: 'Gasthaus Möslinger',
+			address: 'Stuwerstrasse 13, 1020 Wien',
+			image: 'https://www.delinski.at/_media/images/_at/restaurants/detail/gasthaus-moeslinger-2.cache1410184679.jpg',
 			rating: 4,
 		}
 	];
 	return ReS(res, companies);
 };
 module.exports.getCompanies = getCompanies;
+
+const checkDateAvailability = async function(req, res){
+
+	const userDateReservation = req.body.date;
+	const companyIdReservation = req.body.companyId;
+
+	//moment(userDateReservation, "DD-MM-YYYY").format(DATE_FORMAT)
+
+	return ReS(res, companies);
+};
+module.exports.checkDateAvailability = checkDateAvailability;
--- routes/v1/botsRoutes.js
+++ routes/v1/botsRoutes.js
@@ -1,6 +1,7 @@
 const BotController 	           = require('../../controllers/bot.controller');
 module.exports = function (router) {
-    router.post('/hello',          BotController.hello);
-    router.get('/companies',       BotController.getCompanies);
+    router.post('/hello',                 BotController.hello);
+    router.get('/companies',              BotController.getCompanies);
+    router.post('/company/checkDate',       BotController.checkDateAvailability);
     return router;
 };
